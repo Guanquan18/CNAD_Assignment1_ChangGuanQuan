cbd6d0fb40a5992612966f94d86f5d11732ef2e7
Guanquan18 <s10257825@connect.np.edu.sg>
2024-12-09 15:40:16 +0800

    Added Bat script to run multiple servers and cleaned up go mod modules

--- run_servers.bat
+++ run_servers.bat
@@ -7,5 +7,17 @@ echo Starting user service...
 cd ..\user
 start cmd /k "go run user.go"
 
-echo Both services are running in separate windows.
+echo Starting vehicle service...
+cd ..\vehicle
+start cmd /k "go run vehicle.go"
+
+echo Starting reservation service...
+cd ..\reservation
+start cmd /k "go run reservation.go"
+
+echo Starting billing service...
+cd ..\billing
+start cmd /k "go run billing.go"
+
+echo Services are running in separate windows.
 pause
